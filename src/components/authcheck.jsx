ae785a18156bf5152e72acef5eabdf1c0512fde6
sachin sahu <sachinsahu19004@gmail.com>
2025-11-25 17:39:39 +0530

    eye button attached and final commit

--- src/components/authcheck.jsx
+++ src/components/authcheck.jsx
@@ -12,6 +12,8 @@ function Authcheck() {
         email:"",
         password:""
     });
+    const[hide, setHide] = useState(true);
+
     const navigate = useNavigate();
     const handleUserSubmit = (e) =>{
         if(!signup.Username || !signup.email || !signup.password){
@@ -90,15 +92,23 @@ function Authcheck() {
                         <h3 className='text-center mb-4 text-white text-left'>Sign Up</h3>
                         <input type="text" name='Username' value={signup.Username} onChange={(e)=> setSignup({...signup, Username:e.target.value})} id="sign-Username" className=' rounded-0 mb-3' placeholder='Your Name' required />
                         <input type="email" name='email' value={signup.email} onChange={(e) => setSignup({...signup, email:e.target.value})} id="sign-email" className=' mb-3 rounded-0' placeholder='Your Email' required />
-                        <input type="password" name='password' value={signup.password} onChange={(e) => setSignup({...signup, password:e.target.value})} id="sign-password" className=' mb-3 rounded-0' rows="1" placeholder='Your Password' required />
-                        <p className="text-danger bold">* Please Login After Sign Up</p>
+                        <div className="show-div position-relative">
+                            <input type={hide ? "password" : "text" } name='password' value={signup.password} onChange={(e) => setSignup({...signup, password:e.target.value})} id="sign-password" className=' mb-3 rounded-0 ' rows="1" placeholder='Your Password' required />
+                            <i style={{right: '10px', top: '30%'}} onClick={() => setHide(!hide)} className={`text-white fa-solid ${hide? "fa-eye-slash": "fa-eye"} position-absolute`}></i>
+                        </div>    
+                        <p className="text-black text-bold">* Please Login After Sign Up</p>
                         <button style={{backgroundColor: '#f35525', border: 'none'}} onClick={handleUserSubmit} type="submit" className='btn mt-1 btn-primary'>Sign Up</button>
-                        <br /> <br /> <a onClick={()=> setLogin(true)} className='form-link text-white' htmlFor="#">Already have an account? Login</a>
+                        <br /> <br /> <a onClick={()=> setLogin(true)} className='form-link text-white' htmlFor="#" >Already have an account? Login</a>
                     </form> :
                     <form action="" key="signin" onSubmit={handleSignIn} className='p-5 user-form rounded'>
                         <h3 className='text-center mb-4 text-white text-left'>Sign In</h3>
                         <input type="email" name='email' value={signin.email} onChange={(e) => setSignin({...signin, email:e.target.value})} id="sign-email" className=' mb-3 rounded-0' placeholder='Your Email' required />
-                        <input type="password" name='password' value={signin.password}   onChange={(e) => setSignin({...signin, password:e.target.value})} id="sign-password" className=' mb-3 rounded-0' rows="1" placeholder='Your Password' required />
+
+                        <div className="show-div position-relative">
+                            <input type={hide ? "password" : "text" } value={signin.password} onChange={(e) => setSignin({...signin, password:e.target.value})} id="sign-password" className=' mb-3 rounded-0 ' rows="1" placeholder='Your Password' required />
+                            <i style={{right: '10px', top: '30%'}} onClick={() => setHide(!hide)} className={`text-white fa-solid ${hide? "fa-eye-slash": "fa-eye"} position-absolute`}></i>
+                        </div>    
+
                         <button style={{backgroundColor: '#f35525', border: 'none'}} onClick={handleUserlogin} type="submit" className='btn mt-1 btn-primary'>Sign In</button> <br />
                         <br /><a onClick={()=> setLogin(false)} className='form-link text-white' htmlFor="#">Don't have an account? Sign Up</a>
                     </form>
